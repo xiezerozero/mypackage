23d2cb7c0955f32b01695bc8b2ab0633c1b010ca
xielin <xielin@51dianshijia.com>
2023-08-03 11:20:07 +0800

    gzip compact

--- demo/demo.go
+++ demo/demo.go
@@ -1,42 +1,121 @@
 package main
 
 import (
+	"bytes"
+	"compress/gzip"
 	"fmt"
-	_ "github.com/xiezerozero/mypackage/initTest"
+	"github.com/go-redis/redis/v7"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
 )
 
-type Person struct {
-	age int
+func readFile(fileName string) string {
+	b, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		fmt.Println("readFile fail: ", err.Error())
+	}
+	fmt.Println("read bytes: ", len(b))
+	return string(b)
 }
 
-func (p Person) howOld() int {
-	return p.age
+func compress() {
+	originalStr := readFile("compact_data.json")
+
+	// 压缩字符串
+	var compressedData bytes.Buffer
+	writer := gzip.NewWriter(&compressedData)
+	_, err := writer.Write([]byte(originalStr))
+	if err != nil {
+		fmt.Println("Failed to compress data:", err)
+		return
+	}
+	writer.Close()
+
+	redisErr := connectLocalRedis().Set("test", compressedData.Bytes(), 0).Err()
+	if redisErr != nil {
+		fmt.Println("redis set fail: ", redisErr.Error())
+	}
 }
 
-func (p *Person) growUp() {
-	p.age += 1
+func decompress() {
+	b, err := connectLocalRedis().Get("test").Bytes()
+	if err != nil {
+		fmt.Println("redis get fail: ", err.Error())
+		return
+	}
+	fmt.Println("read bytes: ", len(b))
+	reader, err := gzip.NewReader(bytes.NewBuffer(b))
+	if err != nil {
+		fmt.Println("Failed to create gzip reader:", err)
+		return
+	}
+	decompressedData, err := ioutil.ReadAll(reader)
+	if err != nil {
+		fmt.Println("Failed to decompress data:", err)
+		return
+	}
+	reader.Close()
+	fmt.Println("Decompressed data:", string(decompressedData))
+}
+
+func requestGzip() {
+	client := new(http.Client)
+
+	request, err := http.NewRequest("GET", "http://127.0.0.1:8198/3rd/gprograms?token=d1606c8aca9fab32583d1b26d8b1fe3d&from=171066&tm=1690859374503&date=20230731&cid=zj-ckvz4pkryuio", nil)
+	request.Header.Add("Accept-Encoding", "gzip")
+
+	resp, err := client.Do(request)
+	if err != nil {
+		fmt.Println("Failed to get url:", err)
+		return
+	}
+	defer resp.Body.Close()
+	fmt.Println(resp.Header)
+	rawByte, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println("read raw bytes: ", len(rawByte))
+	var reader io.ReadCloser
+	switch resp.Header.Get("Content-Encoding") {
+	case "gzip":
+		//reader, err = gzip.NewReader(resp.Body)
+		reader, err = gzip.NewReader(bytes.NewReader(rawByte))
+		defer reader.Close()
+	default:
+		fmt.Println("not gzip")
+		//reader = resp.Body
+		reader = io.NopCloser(bytes.NewReader(rawByte))
+	}
+
+	n, _ := io.Copy(os.Stdout, reader) // print html to standard out
+	fmt.Println("ungzip read bytes: ", n)
+}
+
+func UnGzip(compressContent string) ([]byte, error) {
+	zr, err := gzip.NewReader(bytes.NewReader([]byte(compressContent)))
+	if err != nil {
+		return nil, err
+	}
+	defer zr.Close()
+	decompressedData, err := ioutil.ReadAll(zr)
+	if err != nil {
+		fmt.Println("Failed to decompress data:", err)
+		return nil, err
+	}
+	return decompressedData, nil
 }
 
 func main() {
-	// qcrao 是值类型
-	qcrao := Person{age: 18}
+	requestGzip()
+}
 
-	// 值类型 调用接收者也是值类型的方法
-	fmt.Println(qcrao.howOld())
-
-	// 值类型 调用接收者是指针类型的方法
-	qcrao.growUp()
-	fmt.Println(qcrao.howOld())
-
-	// ----------------------
-
-	// stefno 是指针类型
-	stefno := &Person{age: 100}
-
-	// 指针类型 调用接收者是值类型的方法
-	fmt.Println(stefno.howOld())
-
-	// 指针类型 调用接收者也是指针类型的方法
-	stefno.growUp()
-	fmt.Println(stefno.howOld())
+func connectLocalRedis() *redis.Client {
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "47.95.250.60:6799",
+		Password: "reEW#@323Vfeoiv0lkaaa",
+		DB:       11,
+	})
+	pong, err := rdb.Ping().Result()
+	fmt.Println(pong, err)
+	return rdb
 }
--- go.mod
+++ go.mod
@@ -3,12 +3,22 @@ module github.com/xiezerozero/mypackage
 go 1.14
 
 require (
-	codeup.aliyun.com/5f3127a8d248146039339d67/common v1.0.0 // indirect
 	github.com/bamzi/jobrunner v1.0.0
-	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/go-redis/redis/v7 v7.4.1 // indirect
+	github.com/golang/protobuf v1.5.2
+	github.com/google/go-cmp v0.5.6 // indirect
+	github.com/json-iterator/go v1.1.12
+	github.com/kr/pretty v0.3.0 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/rogpeppe/go-internal v1.8.0 // indirect
 	github.com/stretchr/objx v0.1.1 // indirect
 	github.com/stretchr/testify v1.7.0
+	golang.org/x/net v0.0.0-20211112202133-69e39bad7dc2 // indirect
+	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/genproto v0.0.0-20211208223120-3a66f561d7aa // indirect
 	google.golang.org/grpc v1.43.0
 	google.golang.org/protobuf v1.27.1
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
